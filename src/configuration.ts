2bed3d67c460b7cc8297e370fc5e9fd87cc55ad7
Ofer Mendelevitch <ofermend@gmail.com>
2024-04-26 07:17:33 -0700

    updated title

--- src/configuration.ts
+++ src/configuration.ts
@@ -3,7 +3,7 @@ import { Config } from "./contexts/ConfigurationContext";
 export const configuration: Config = {
   corpusId: "265",
   customerId: "1526022105",
-  appTitle: "Search HN",
+  appTitle: "Search HackerNews",
   apiKey: "zqt_WvU_2YIBRMve7vmQP3h70deqDOgYM0W5VpYeXQ",
   endpoint: "api.vectara.io",
   questions: ["what was announced in GTC 2024?", "bash power user", "Open source LLM", "What is a vector database?"]
