7cc911843f2ec6e6f1cf1601b901a3a1732cd8b8
adeelehsan <aadeel.ehsan@gmail.com>
2024-07-08 08:40:12 -0700

    moved the config to .env

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,13 +1,10 @@
 import { Config } from "./contexts/ConfigurationContext";
 
 export const configuration: Config = {
-  corpusId: "265",
-  customerId: "1526022105",
-  appTitle: "Search HackerNews",
-  apiKey: "zqt_WvU_2YIBRMve7vmQP3h70deqDOgYM0W5VpYeXQ",
-  endpoint: "api.vectara.io",
-  questions: [
-    "What is Gemini-Flash?", "When will GPT-5 be released?", 
-    "What is Llama3?", "can I win the Arc-AGI competition?"
-  ]
+  corpusId: process.env.REACT_APP_CORPUS_ID,
+  customerId: process.env.REACT_APP_CUSTOMER_ID,
+  appTitle: process.env.REACT_APP_APP_TITLE,
+  apiKey: process.env.REACT_APP_API_KEY,
+  endpoint: process.env.REACT_APP_ENDPOINT,
+  questions: process.env.REACT_APP_QUESTION?.split(',') || []
 };
